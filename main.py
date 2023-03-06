78e2eae49ac9af20b3dc9aa9a15f5cac49895a73
shilodaum <shilodaum@gmail.com>
2023-03-06 12:15:46 +0200

    new

--- main.py
+++ main.py
@@ -78,7 +78,7 @@ def start(update: Update, context: CallbackContext) -> None:
 
 
 # bot handling
-bot_key=os.environ.get('BOT_KEY')
+bot_key= '5101989049:AAEdqv8WxlnXd7V5_OEfzC5ueMvo1c1WwFo'  # os.environ.get('BOT_KEY')
 updater = Updater(bot_key)
 print('created updater')
 updater.dispatcher.add_handler(CommandHandler('start', start))
